c19d8a5909d519b8d7ed84f2b8328801f2d3de6d
Alessandro Arosio <alessandro.arosio1@gmail.com>
2021-01-10 16:41:06 +0000

    adding BeerService

---
+++ src/main/java/guru/springframework/msscbeerservice/services/BeerServiceImpl.java
@@ -0,0 +1,42 @@
+package guru.springframework.msscbeerservice.services;
+
+import guru.springframework.msscbeerservice.repositories.BeerRepository;
+import guru.springframework.msscbeerservice.web.controller.NotFoundException;
+import guru.springframework.msscbeerservice.web.mappers.BeerMapper;
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class BeerServiceImpl implements BeerService {
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
+    @Override
+    public BeerDto getById(UUID beerId) {
+        return beerMapper.beerToBeerDto(
+                beerRepository.findById(beerId).orElseThrow(NotFoundException::new));
+    }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return beerMapper.beerToBeerDto(
+                beerRepository.save(
+                        beerMapper.dtoToBeer(beerDto)));
+    }
+
+    @Override
+    public BeerDto updateBeer(UUID beerId, BeerDto beerDto) {
+        var beer = beerRepository.findById(beerId).orElseThrow(NotFoundException::new);
+
+        beer.setBeerName(beerDto.getBeerName());
+        beer.setBeerStyle(beerDto.getBeerStyle().name());
+        beer.setPrice(beerDto.getPrice());
+        beer.setUpc(beerDto.getUpc());
+
+        return beerMapper.beerToBeerDto(beerRepository.save(beer));
+    }
+}
