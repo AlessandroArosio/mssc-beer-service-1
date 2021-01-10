ce38b6aa0c6a55351673395b4df5b9edeb5197f1
Alessandro Arosio <alessandro.arosio1@gmail.com>
2021-01-10 16:49:10 +0000

    updating UPC from Long to String

--- src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
@@ -15,6 +15,10 @@ import java.math.BigDecimal;
 public class BeerLoader implements CommandLineRunner {
     private final BeerRepository beerRepository;
 
+    public static final String BEER_1_UPC = "0631234200036";
+    public static final String BEER_2_UPC = "0631234300019";
+    public static final String BEER_3_UPC = "0083783375213";
+
     @Override
     public void run(String... args) throws Exception {
         loadBeer();
@@ -28,7 +32,7 @@ public class BeerLoader implements CommandLineRunner {
                     .beerStyle("IPA")
                     .quantityToBrew(200)
                     .minOnHand(12)
-                    .upc(2462457347L)
+                    .upc(BEER_1_UPC)
                     .price(new BigDecimal("12.95"))
                     .build());
 
@@ -37,7 +41,16 @@ public class BeerLoader implements CommandLineRunner {
                     .beerStyle("PALE ALE")
                     .quantityToBrew(200)
                     .minOnHand(12)
-                    .upc(2462457347L)
+                    .upc(BEER_2_UPC)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("No hammers on the bar")
+                    .beerStyle("PALE ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(BEER_3_UPC)
                     .price(new BigDecimal("11.95"))
                     .build());
 
