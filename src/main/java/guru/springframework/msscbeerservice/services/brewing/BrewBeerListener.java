8d262b921ad587aaabf69ea08bce2a4ab54d5451
Alessandro Arosio <alessandro.arosio1@gmail.com>
2021-01-20 17:31:23 +0000

    Fix Jackson configuration

--- src/main/java/guru/springframework/msscbeerservice/services/brewing/BrewBeerListener.java
+++ src/main/java/guru/springframework/msscbeerservice/services/brewing/BrewBeerListener.java
@@ -10,6 +10,8 @@ import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -17,6 +19,7 @@ public class BrewBeerListener {
     private final BeerRepository beerRepository;
     private final JmsTemplate jmsTemplate;
 
+    @Transactional
     @JmsListener(destination = JmsConfig.BREWING_REQUEST_QUEUE)
     public void listen(BrewBeerEvent event) {
         var beerDto = event.getBeerDto();
